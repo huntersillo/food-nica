7209332127574b761b4233641ac8141bc7ab3765
Julio cAstillo <juliocastillo@Julios-MacBook-Pro.local>
2019-07-10 21:26:27 -0700

    router

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,36 +11,41 @@ class Directory extends React.Component {
     this.state = {
       sections: [
         {
-          title: "nacatamal",
+          title: "hats",
           imageUrl:
             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRONSci07gXZa-8M9EOuVgq4vsq0anIFqBulk9lbsRuOGZ9CnWLCw",
-          id: 1
+          id: 1,
+          linkUrl: "hats"
         },
         {
           title: "cacao drink",
           imageUrl:
             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9hpJ7L87tfSpwnny06kCxCaGI2drbom2F4qVqBlKv9juuRZzuDg",
-          id: 2
+          id: 2,
+          linkUrl: ""
         },
         {
           title: "gallopinto",
           imageUrl:
             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReYjRUZsNv0BgcvovylmwjTVmdAfIuYd4xeJgpD41NGX9gmRL7",
-          id: 3
+          id: 3,
+          linkUrl: ""
         },
         {
           title: "vigoron",
           imageUrl:
             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtRwS1JRsJpisbhs9vJ_b7ud7QHfRpvN0pBZDQhE3VRx9qutzQdg",
           size: "large",
-          id: 4
+          id: 4,
+          linkUrl: ""
         },
         {
           title: "baho",
           imageUrl:
             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyPcgEZrV8oDUGBm4ZLLOTOXbWE74P34ykBcFk6SbkDctUt6R_",
           size: "large",
-          id: 5
+          id: 5,
+          linkUrl: ""
         }
       ]
     };
@@ -49,8 +54,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
