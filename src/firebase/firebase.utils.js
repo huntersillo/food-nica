c5431fbd754e365f198d3c17d62b4a5a40d3e882
Julio cAstillo <juliocastillo@Julios-MacBook-Pro.local>
2019-07-13 00:02:48 -0700

    auth add firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,5 +1,5 @@
 import firebase from "firebase/app";
-// import "firebase/firestore";
+import "firebase/firestore";
 import "firebase/auth";
 const config = {
   apiKey: "AIzaSyDKnheC-1mjEda2Un3tSBWAta0PdRi0yhc",
@@ -13,8 +13,33 @@ const config = {
 
 firebase.initializeApp(config);
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
+
 export const auth = firebase.auth();
-// export const firestore = firestore.firestore();
+export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" });
