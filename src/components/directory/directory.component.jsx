b993d0c58d1db4d5b81caaef1b676f5506f124e4
Julio cAstillo <juliocastillo@Julios-MacBook-Pro.local>
2019-07-14 18:06:04 -0700

    router add

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,65 +1,23 @@
 import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
+import { selectDirectorySections } from "../../redux/directory/directory.selectors";
 
 import MenuItem from "../menu-item/menu-item.component";
 
 import "./directory.styles.scss";
 
-class Directory extends React.Component {
-  constructor() {
-    super();
+const Directory = ({ sections }) => (
+  <div className="directory-menu">
+    {sections.map(({ id, ...otherSectionProps }) => (
+      <MenuItem key={id} {...otherSectionProps} />
+    ))}
+  </div>
+);
 
-    this.state = {
-      sections: [
-        {
-          title: "hats",
-          imageUrl:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRONSci07gXZa-8M9EOuVgq4vsq0anIFqBulk9lbsRuOGZ9CnWLCw",
-          id: 1,
-          linkUrl: "hats"
-        },
-        {
-          title: "cacao drink",
-          imageUrl:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9hpJ7L87tfSpwnny06kCxCaGI2drbom2F4qVqBlKv9juuRZzuDg",
-          id: 2,
-          linkUrl: ""
-        },
-        {
-          title: "gallopinto",
-          imageUrl:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReYjRUZsNv0BgcvovylmwjTVmdAfIuYd4xeJgpD41NGX9gmRL7",
-          id: 3,
-          linkUrl: ""
-        },
-        {
-          title: "vigoron",
-          imageUrl:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtRwS1JRsJpisbhs9vJ_b7ud7QHfRpvN0pBZDQhE3VRx9qutzQdg",
-          size: "large",
-          id: 4,
-          linkUrl: ""
-        },
-        {
-          title: "baho",
-          imageUrl:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyPcgEZrV8oDUGBm4ZLLOTOXbWE74P34ykBcFk6SbkDctUt6R_",
-          size: "large",
-          id: 5,
-          linkUrl: ""
-        }
-      ]
-    };
-  }
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
+});
 
-  render() {
-    return (
-      <div className="directory-menu">
-        {this.state.sections.map(({ id, ...otherSectionProps }) => (
-          <MenuItem key={id} {...otherSectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
-
-export default Directory;
+export default connect(mapStateToProps)(Directory);
